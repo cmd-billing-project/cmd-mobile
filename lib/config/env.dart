d2508a3d442261e96375a619d70491d5f2ea5ec8
Frostzeichen <cruztjomar@gmail.com>
2026-03-04 12:35:23 +0000

    feat: change BE

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,4 +1,3 @@
 class Env {
-  // Android emulator -> your computer localhost
-  static const String apiBaseUrl = "http://10.0.2.2:3000";
+  static const String apiBaseUrl = "https://api.cmdunlifibermax.com";
 }
