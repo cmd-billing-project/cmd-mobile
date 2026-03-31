6cc7be57e6328800892f4c1b5ebd13e7d7109a87
Frostzeichen <cruztjomar@gmail.com>
2026-04-01 00:06:31 +0800

    feat: add online payment gateway checkout

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,4 +1,7 @@
 class Env {
   // Android emulator -> your computer localhost
   static const String apiBaseUrl = "http://10.0.2.2:3000";
+
+  // Web app base URL for payment gateway redirect URLs
+  static const String webBaseUrl = "https://app.cmdcablevision.com";
 }
